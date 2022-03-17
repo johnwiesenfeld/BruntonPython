a98b1c7c84adde097fcccec4e895806214b5af71
johnwiesenfeld <jawiesenfeld@gmail.com>
2022-03-17 11:47:47 -0700

    Update Brunton.py

--- Brunton.py
+++ Brunton.py
@@ -1,5 +1,16 @@
+import numpy as np
+import datetime as dt
+
 class Brunton:
-
     def __init__(self):
-
+        number = 0
+        oldtag = 0
+        newtag = 0
+        serialnumber = 0
+        checkedOut = False
+        needsRepair = False
+        missing = False
+        name = ''
+        checkoutDate = dt.date
+        checkinDate = dt.date
         
